bce338437b650a90b21a42bcc3f155a4d8c36215
Conanap <albionapc@gmail.com>
2020-04-08 19:36:35 -0400

    removed api key, added licensing information, change buffer size from 16kb -> 32mb.

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
+/*
+Written by Albion Fung
+
+Refs:
+1. https://gabrieltanner.org/blog/dicord-music-bot
+2. https://www.online-tech-tips.com/fun-stuff/how-to-make-your-own-discord-music-bot/
+3. https://dev.to/galnir/how-to-write-a-music-command-using-the-discord-js-library-462f
+4. https://stackoverflow.com
+5. https://github.com (issues)
+
+Reqs: Make sure to have a file config.json in the project's root folder. It must contain:
+- prefix for the bot
+- developerPrefix for the testing bot (can be the same as above prefix if not using a test bot)
+- token for the bot
+- developerToken for the testing bot (can be the same as above token if not using a test bot)
+- API Key for Youtube API v3
+
+You may freely distribute and use my code as long as your code is:
+- open source
+- credit me
+- credit above refs when using my code
+- do not directly charge customers for using an application that uses this code base *
+- have this same clause for the distribution and integration of your code
+
+*: The code may still be used in a commercial operation as long as it is offered free.
+   For example, you may make use of this code in an operation that makes a profit off of
+   advertisements, as long as you do not charge users to pay for this code's functionality
+   - including the functionality of this code "free" to certain tiers of purchase but not
+   others is considered charging the user directly.
+*/
+
 const args = process.argv.slice(2);
 const isDev = args.indexOf('-dev') === -1 ? false : true;
 const isDebug = args.indexOf('-debug') === -1 ? false : true;
@@ -8,7 +39,8 @@ let {
     prefix,
     developerPrefix,
     token,
-    developerToken
+    developerToken,
+    apiKey
 } = require('./config.json');
 
 const otPrefixes = ['!', '>', isDev ? prefix : developerPrefix];
@@ -18,7 +50,6 @@ const client = new Discord.Client();
 isDev ? client.login(developerToken) : client.login(token);
 
 const fetch = require("node-fetch");
-const apiKey = "AIzaSyAHrrwsRNbBIGELxhlu9qZwdT5NImBSbDE";
 const url = "https://www.googleapis.com/youtube/v3/search?part=id&key=" + apiKey + "&q=";
 const vurl = "https:www.youtube.com/watch?v=";
 
@@ -176,7 +207,7 @@ function play(guild, song) {
 
     console.log(song.url);
     dispatcher = serverQueue.connection
-        .play(ytdl(song.url, { filter: 'audioonly'}))
+        .play(ytdl(song.url, { filter: 'audioonly', quality: 'highestaudio', highWaterMark: 1 << 25 }))
         .on("finish", () => {
         serverQueue.songs.shift();
         play(guild, serverQueue.songs[0]);
